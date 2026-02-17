46803e699bdb8e26aeeced40c60504d26ffbaf0a
Dwayne Jackson II <81774183+DwayneJacksonII@users.noreply.github.com>
2026-02-17 01:18:29 -0500

    Update SCCMWinAzureVM.ps1 with new comments and structure
    Spacing

--- SCCMWinAzureVM.ps1
+++ SCCMWinAzureVM.ps1
@@ -1,15 +1,15 @@
-//------------------------------------------------------------------------------
-//
-// Copyright © 2026 Microsoft Corporation.  All rights reserved.
-//
-// THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
-// WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
-// FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR
-// RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
-//
-//------------------------------------------------------------------------------
+<#------------------------------------------------------------------------------
 
+ Copyright © 2026 Microsoft Corporation.  All rights reserved.
+
+ THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED "AS IS" WITHOUT
+ WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+ LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+ FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR
+ RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+
+#------------------------------------------------------------------------------
+#>
 
 
 <#
@@ -161,4 +161,5 @@ try {
     Write-Log "============================================" "ERROR"
     exit 1
 }
+
 #endregion
